a6401671bc8b4b7cd6f5c98ef27cf0c928610c5a
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-05-09 12:39:04 +0600

    mongoDb pass and name are remove

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const port = process.env.PORT || 5000;
 //set medlewar
 app.use(cors());
 app.use(express.json());
-// pass ohNZOIooGkGK2avp
-// name chocolateManagement
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.5a8lj4m.mongodb.net/?retryWrites=true&w=majority`;
-
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
